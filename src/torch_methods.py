776147b9841956f4b652ba96fcd284960372f45b
oskarruo <oskar.ruokolainen@gmail.com>
2026-04-08 00:05:40 +0300

    formatting

--- src/torch_methods.py
+++ src/torch_methods.py
@@ -35,24 +35,26 @@ class ExpvalFunction(torch.autograd.Function):
             f_plus = circuit.op_expval(plus, ops)
             f_minus = circuit.op_expval(minus, ops)
 
-            grad_i = (f_plus - f_minus)
+            grad_i = f_plus - f_minus
             grads.append(grad_i)
 
-        jacobian = torch.tensor(np.array(grads).T, dtype=params.dtype, device=grad_output.device)
+        jacobian = torch.tensor(
+            np.array(grads).T, dtype=params.dtype, device=grad_output.device
+        )
         grad_params = grad_output @ jacobian
 
         return grad_params, None, None, None
-    
+
     def backward_not(ctx, grad_output):
         (params,) = ctx.saved_tensors
         circuit = ctx.circuit
         ops = ctx.ops
         params_np = params.detach().numpy()
-        
+
         # Because the gate is 2*theta, the shift must be scaled
         # For a gate Exp(-i * a * theta), shift is pi / (2 * a)
         shift = np.pi / 4  # Adjusted for the '2' in your cos(2*theta)
-        
+
         grads = []
         for i in range(len(params_np)):
             plus = params_np.copy()
@@ -64,13 +66,14 @@ class ExpvalFunction(torch.autograd.Function):
             f_minus = circuit.op_expval(minus, ops)
 
             # The factor at the end (1.0) depends on the '2' in 2*theta
-            grad_i = (f_plus - f_minus) 
+            grad_i = f_plus - f_minus
             grads.append(grad_i)
 
         jacobian = torch.tensor(np.array(grads).T, dtype=torch.float32)
         grad_params = grad_output @ jacobian
 
         return grad_params, None, None
+
 
 def expvals_torch(params, circuit, ops):
     return ExpvalFunction.apply(params, circuit, ops)
@@ -81,6 +84,8 @@ def mmd_loss_torch(params, circuit, ground_truth, sigma, n_ops, ops):
     expvals = expvals_torch(params, circuit, ops)
 
     data_vals = 1 - 2 * ((ground_truth @ ops.T) % 2)
-    tr_data = torch.tensor(data_vals.mean(axis=0), dtype=expvals.dtype, device=expvals.device)
+    tr_data = torch.tensor(
+        data_vals.mean(axis=0), dtype=expvals.dtype, device=expvals.device
+    )
 
     return torch.mean((expvals - tr_data) ** 2)
