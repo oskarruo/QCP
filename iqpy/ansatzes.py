fdf57a50b67998b6d364aa193d49121c5508aa61
oskarruo <oskar.ruokolainen@gmail.com>
2026-04-16 21:58:14 +0300

    added nearest neighbour ansatz based on graph

--- iqpy/ansatzes.py
+++ iqpy/ansatzes.py
@@ -1,3 +1,7 @@
+import itertools
+import networkx as nx
+
+
 def nearest_neighbour_IQP_ansatz(n_qubits):
     """
     Nearest-neighbour IQP ansatz.
@@ -41,5 +45,68 @@ def fully_connected_IQP_ansatz(n_qubits):
     for i in range(n_qubits):
         for j in range(i + 1, n_qubits):
             gates.append([[i, j]])
+
+    return gates
+
+
+def nodes_within_distance(G, source, distance):
+    """
+    Given a graph G, returns all nodes on within a specified distance of a source node.
+
+    Params:
+        G: A networkx graph object
+        source: source node
+        distance: maximum distance from source node
+
+    Returns:
+        list of nodes within specified distance
+    """
+
+    current_level = {source}
+    visited = {source}
+    all_nodes_within_distance = set(current_level)
+
+    for _ in range(distance):
+        next_level = set()
+        for node in current_level:
+            for neighbor in G.neighbors(node):
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    next_level.add(neighbor)
+        current_level = next_level
+        all_nodes_within_distance.update(current_level)
+
+    all_nodes_within_distance.discard(source)
+    return list(all_nodes_within_distance)
+
+
+def nearest_neighbour_from_graph_IQP_ansatz(G, distance: int, max_weight=2):
+    """
+    For every node on a graph G, find all other nodes within a specified distance, and create all gates
+    up to a maximum weight from these nodes.
+
+    Params:
+        G: networkx graph that determines the distances, or a path reference to a networkx adjacency list file
+        distance: maximum distance to consider
+        max_weight: maximum weight of the generators of the gates
+
+    Returns:
+        list of lists specifying the gates
+    """
+
+    if isinstance(G, str):
+        G = nx.read_adjlist(G, nodetype=int)
+
+    gates = []
+    for source in list(G.nodes):
+        neighbours = nodes_within_distance(G, source, distance)
+        for weight in range(0, max_weight):
+            for combo in itertools.combinations(neighbours, weight):
+                new_gate = list(combo) + [source]
+                new_gate = [int(elem) for elem in new_gate]
+                perms = list(itertools.permutations(new_gate))
+                perms = [[list(perm)] for perm in perms]
+                if not any([perm in gates for perm in perms]):
+                    gates.append([new_gate])
 
     return gates
